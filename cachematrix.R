02b5e14c326d561942455afb4fee0b4896fa4974
nalefu <nalefu@gmail.com>
2015-02-18 20:46:04 -0500

    bjhbjh
    bkjbkb

--- cachematrix.R
+++ cachematrix.R
@@ -1,21 +1,28 @@
+## "makeCacheMatrix" and "cacheSolve" are used to cache the inverse of a matrix
+
 makeCacheMatrix <- function(z = matrix()) {
+   ## setting the value of the matrix
     y <- NULL 
     x <- NULL 
     setmatrix <- function(y) { 
         z <<- x
         y <<- NULL 
     }
+    ## getting the value of the matrix
     getmatrix <- function() 
         z 
+    ## setting the value of the matrix
     setinverse <- function(solve) 
         y <<- solve 
     getinverse <- function() 
         y 
+    ##getting the inverse of the matrix
     list(setmatrix = setmatrix, getmatrix = getmatrix, 
          setinverse = setinverse,
          getinverse = getinverse)
 }
 
+## "cacheSolve" returns the inverse of a matrix. The function checks if the inverse has already been computed and if it hasn't been then it computes the inverse.  
 cacheSolve <- function (z=matrix(), ...) {
     y <- z$getinverse() 
     if(!is.null(y)){ 
